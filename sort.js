d7c62183c3540cd534b0a1f25cfd3e5d9d47d701
selenci <klancar.matej@gmail.com>
2021-04-26 19:43:32 +0200

    nic ne dela lmao

--- sort.js
+++ sort.js
@@ -1,6 +1,7 @@
 let levi = 0, desni = 1, curr;
 let pesmi = [];
 var queries = {};
+let val = 0;
 function swap(arr, i, j){
     let tmp = arr[i];
     arr[i] = arr[j];
@@ -18,40 +19,65 @@ $.get(`https://musicbrainz.org/ws/2/release/${queries.id}?inc=recordings&fmt=jso
         pesmi.push(song.title);
     }
     console.log(pesmi);
-    $("#levi").text(pesmi[levi]);
-    $("#desni").text(pesmi[1]);
-    curr = pesmi[1];
+    quickSort(pesmi, 0, pesmi.length - 1);
 })
 
-function insertion(val)
-{
-    console.log(levi, desni);
-    if(levi >= 0 && val > 0)
-    {
-        pesmi[levi + 1] = pesmi[levi];
-        --levi;
-    }
-    if(val < 0 || levi < 0)
-    {
-        pesmi[levi + 1] = curr;
-        ++desni;
-        levi = desni - 1;
+const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+async function paPridobimoToSpremenljivko(){
+    while(!val)
+        await sleep(100);
+    let v = val;
+    val = 0;
+    return v;
+}
 
-        if(desni < pesmi.length)
-            curr = pesmi[desni];
+async function partition(items, left, right) {
+    var pivot   = items[Math.floor((right + left) / 2)], //middle element
+        i       = left, //left pointer
+        j       = right; //right pointer
+    while (i <= j) {
+        $("#levi").text(pesmi[i]);
+        $("#desni").text(pivot);
+        let value = await paPridobimoToSpremenljivko();
+        while (value > 0) {
+            i++;
+            $("#levi").text(pesmi[i]);
+            $("#desni").text(pivot);
+            value = await paPridobimoToSpremenljivko();
+        }
+        $("#levi").text(pivot);
+        $("#desni").text(pesmi[j]);
+        value = await paPridobimoToSpremenljivko();
+        while (value > 0) {
+            j--;
+            $("#levi").text(pivot);
+            $("#desni").text(pesmi[j]);
+            value = await paPridobimoToSpremenljivko();
+        }
+        if (i <= j) {
+            swap(items, i, j); //sawpping two elements
+            i++;
+            j--;
+        }
+        console.log(pesmi);
     }
+    return i;
+}
 
-    if(desni >= pesmi.length)
-    {
-        $("#levi").remove();
-        $("#desni").remove();
-        izpis();
+function quickSort(items, left, right) {
+    var index;
+    if (items.length > 1) {
+        index = partition(items, left, right); //index returned from partition
+        if (left < index - 1) { //more elements on the left side of the pivot
+            quickSort(items, left, index - 1);
+        }
+        if (index < right) { //more elements on the right side of the pivot
+            quickSort(items, index, right);
+        }
     }
-    else{
-        $("#levi").text(pesmi[levi]);
-        $("#desni").text(curr);
-    }
-    console.log(pesmi);
+    izpis();
 }
 
 function izpis(){
